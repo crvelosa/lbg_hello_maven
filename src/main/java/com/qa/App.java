de2d4418bd9c4b721f32b289c46d780827b22c7b
Carlos <velosa@yahoo.com>
2026-04-28 16:16:13 +0100

    EDIT: release build 2.0

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Carlos"));
+        System.out.println(sayHelloToSomeone("Emma"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
